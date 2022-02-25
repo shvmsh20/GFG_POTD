035d2922025d2efeb25fd1f945689797ef9c2422
shvmsh20 <itsshivamsharma21@gmail.com>
2022-02-26 01:37:37 +0530

    update: 25 Feb 2022

--- Robots.cpp
+++ Robots.cpp
@@ -14,3 +14,25 @@ string moveRobots(string s1, string s2){
         }
         return "Yes";
     }
+
+    string moveRobots(string s1, string s2){
+        // code here 
+        int n = s1.size(), i;
+        queue<pair<char, int>> q;
+        for(i=0; i<n; i++){
+            if(s2[i]!='#'){
+                q.push({s2[i], i});
+            }
+        }
+        for(i=0; i<n; i++){
+            if(!q.empty() && s1[i]!='#'){
+                pair<char, int> p = q.front();
+                q.pop();
+                if(p.first!=s1[i]) return "No";
+                else if(s1[i]=='B' && i>p.second) return "No";
+                else if(s1[i]=='A' && i<p.second) return "No";
+            }
+        }
+        if(q.empty()) return "Yes";
+        else return "No";
+    }
