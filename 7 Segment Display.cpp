58611fd963913988dca49dbf52e5954d2935bda4
shvmsh20 <itsshivamsharma21@gmail.com>
2022-03-13 01:07:32 +0530

    update:10 March 2022

--- 7 Segment Display.cpp
+++ 7 Segment Display.cpp
@@ -1,3 +1,4 @@
+//M-1
 string sevenSegments(string S, int N) {
         // code here
          S = S.substr(0, N);
@@ -18,3 +19,56 @@ string sevenSegments(string S, int N) {
         }
         return x;
     }
+
+//M-2
+string sevenSegments(string S, int N) {
+        // code here
+        vector<int> v = {6,2,5,5,4,5,6,3,7,5};
+        string ans="";
+        int count=0;
+        for(int i=0; i<S.size(); i++){
+            count+= v[S[i]-'0'];
+        }
+        vector<int> output(N, 2);
+        count-= 2*N;
+        int i=0;
+        while(i<N && count>=4){
+            count-=4;
+            output[i++] = 6;
+        }
+        
+        i=N-1;
+        while(count>0){
+            if(count>7-output[i]){
+                output[i]=7;
+                count-= 7-output[i];
+                i--;
+            }else{
+                output[i]+=count;
+                count=0;
+            }
+        }
+        for(int i=0; i<N; i++){
+            switch(output[i]){
+                case 2:
+                    ans+= "1";
+                    break;
+                case 3:
+                    ans+= "7";
+                    break;
+                case 4:
+                    ans+= "4";
+                    break;
+                case 5:
+                    ans+= "2";
+                    break;
+                case 6:
+                    ans+= "0";
+                    break;
+                case 7:
+                    ans+= "8";
+                    break;    
+            }
+        }
+        return ans;
+    }    
