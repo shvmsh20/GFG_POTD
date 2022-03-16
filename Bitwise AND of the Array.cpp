b470ca0c9b197c49cc6ebf2a5c6253dca76b9573
shvmsh20 <itsshivamsharma21@gmail.com>
2022-03-16 22:10:54 +0530

    update: 15 March 2022

--- Bitwise AND of the Array.cpp
+++ Bitwise AND of the Array.cpp
@@ -1,3 +1,4 @@
+//M-1
 int count(int N, vector<int> A,int X)
     {
         // code here
@@ -32,4 +33,27 @@ int count(int N, vector<int> A,int X)
         }
 
       return ans;
+    }
+
+//M-2    
+int count(int N, vector<int> A,int X)
+    {
+        // code here
+        int ans=INT_MAX, bitSetInX=0;
+        for(int i=30; i>=0; i--){
+            if((1<<i) & X){
+                bitSetInX = bitSetInX | (1<<i);
+            }else{
+                //golden opportunity
+                int temp = bitSetInX | (1<<i);
+                int modify=0;
+                for(auto x: A){
+                    if((temp & x) != temp){
+                        modify++;
+                    }
+                }
+                ans = min(ans, modify);
+            }
+        }
+        return ans;
     }
