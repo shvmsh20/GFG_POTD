8f7b50c2eb1e8da8e1413f11716f575d1080f2e4
shvmsh20 <itsshivamsharma21@gmail.com>
2022-06-06 03:16:35 +0530

    5 June 2022

---
+++ June 2022/Count pairs in array divisible by K.cpp
@@ -0,0 +1,16 @@
+long long countKdivPairs(int a[], int n, int k)
+    {
+        //code here
+        unordered_map<int, int> mp;
+        long long  count=0;
+        for(int i=0; i<n; i++){
+            int rem = a[i]%k;
+            if(rem==0 && mp.find(0)!=mp.end()){
+                count+= mp[0];
+            }else if(mp.find(k-rem)!=mp.end()){
+                count+= mp[k-rem];
+            }
+            mp[rem]++;
+        }
+        return count;
+    }
