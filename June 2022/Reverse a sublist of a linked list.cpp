ea618277127bf6562bd88cc4e2a353f30196ffeb
shvmsh20 <itsshivamsharma21@gmail.com>
2022-06-22 00:07:10 +0530

    update: 19 June 2022

--- June 2022/Reverse a sublist of a linked list.cpp
+++ June 2022/Reverse a sublist of a linked list.cpp
@@ -13,3 +13,35 @@ Node* reverseBetween(Node* head, int m, int n)
     }
         return mp[1];
     }
+
+
+    Node* reverseBetween(Node* head, int m, int n)
+    {
+        //code here
+        Node* orgPrev, *curr = head, *prev = NULL, *orgCurr, *next;
+        int pos=1;
+        while(pos<m){
+            prev = curr;
+            curr = curr->next;
+            pos++;
+        }
+        orgPrev = prev;
+        orgCurr = curr;
+        prev = curr;
+        curr = curr->next;
+        pos++;
+        while(pos<=n && curr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            pos++;
+        }
+        orgCurr->next = curr;
+        if(orgPrev){
+            orgPrev->next = prev;
+            return head;
+        }else{
+            return prev;
+        }
+    }
