1c2c54b2553d6df8b5cd093f00e971d65cacfbe9
shvmsh20 <itsshivamsharma21@gmail.com>
2022-08-07 23:01:11 +0530

    7 Aug 2022

---
+++ Aug 2022/Exceptionally odd.cpp
@@ -0,0 +1,8 @@
+int getOddOccurrence(int arr[], int n) {
+        // code here
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            ans = ans^arr[i];
+        }
+        return ans;
+    }
