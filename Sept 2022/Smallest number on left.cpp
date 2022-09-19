e57f4cc97dc4141c7e982e86a9392f6a76891859
shvmsh20 <itsshivamsharma21@gmail.com>
2022-09-19 22:25:20 +0530

    18 Sept 2022

---
+++ Sept 2022/Smallest number on left.cpp
@@ -0,0 +1,15 @@
+vector<int> leftSmaller(int n, int a[]){
+        // code here
+        vector<int> res(n, -1);
+        stack<int> st;
+        for(int i=0; i<n; i++){
+            while(!st.empty() && st.top()>=a[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                res[i] = st.top();
+            }
+            st.push(a[i]);
+        }
+        return res;
+    }
