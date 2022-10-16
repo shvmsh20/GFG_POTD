d294727cf07c9250078c1aa62b955bc206b96380
shvmsh20 <itsshivamsharma21@gmail.com>
2022-10-16 23:04:32 +0530

    1 Oct 2022

---
+++ October 2022/Move Last Element to Front of a Linked List.cpp
@@ -0,0 +1,13 @@
+ListNode *moveToFront(ListNode *head){
+        if(head==nullptr || head->next==nullptr){
+            return head;
+        }
+        ListNode* curr = head, *prev = nullptr;
+        while(curr->next){
+            prev = curr;
+            curr = curr->next;
+        }
+        curr->next = head;
+        prev->next = nullptr;
+        return curr;
+    }
