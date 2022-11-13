19ecb7917a125dd6c623b0ade4f2d5c2adcb5bde
shvmsh20 <itsshivamsharma21@gmail.com>
2022-11-13 22:13:00 +0530

    13 Nov 2022

---
+++ Nov 2022/Fill up buckets.cpp
@@ -0,0 +1,11 @@
+int totalWays(int n, vector<int>capacity) {
+        // code here
+        sort(capacity.begin(), capacity.end());
+        uint64_t ans = 1;
+        uint64_t mod = 1e9 + 7;
+        for(int i = 0; i < n; i++){
+            ans *= (capacity[i] - i) % mod;
+            ans %= mod;
+        }
+        return ans;
+    }
