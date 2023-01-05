2d17344f5efd36439019d43173d0cbd5b7315b9c
shvmsh20 <itsshivamsharma21@gmail.com>
2023-01-05 12:13:39 +0530

    5 Jan 2023

---
+++ Jan 2023/Find the longest string.cpp
@@ -0,0 +1,21 @@
+string longestString(vector<string> &words)
+    {
+        // code here
+        sort(words.begin(), words.end());
+        unordered_map<string, int>ump;
+        for(auto &i : words)    ump[i]++;
+        string s;   int l = 0;
+        for(auto &i : words){
+            string x;    int n = i.size();
+            for(int j = 0; j < n; j++){
+                x += i[j];
+                if(!ump[x]){
+                    n = 0;  break;
+                }
+            }
+            if(n > l){
+                s = i;  l = n;
+            }
+        }
+        return s;
+    }
