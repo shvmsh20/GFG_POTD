b9e4c37d7fcee9d15b3be36e324ef4467c92b50a
shvmsh20 <itsshivamsharma21@gmail.com>
2023-04-10 22:04:56 +0530

    10 April 2023

---
+++ April 2023/Maximum Intersecting Lines.cpp
@@ -0,0 +1,16 @@
+nt maxIntersections(vector<vector<int>> lines, int N) {
+        // Code here 
+        map<int,int> mp;
+        for(auto it:lines){
+            int s=it[0],e=it[1];
+            mp[s]++;
+            mp[e+1]--;
+        }
+        int maxi=1;
+        int presum=0;
+        for(auto it:mp){
+            presum+=it.second;
+            maxi=max(maxi,presum);
+        }
+        return maxi;
+    }
