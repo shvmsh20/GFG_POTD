b6242902e1fd821b7978e5b1adfa3f4df4535c46
shvmsh20 <itsshivamsharma21@gmail.com>
2023-06-11 20:13:40 +0530

    11 June 2023

---
+++ June 2023/Adding Ones.cpp
@@ -0,0 +1,13 @@
+void update(int a[], int n, int updates[], int k)
+    {
+        // Your code goes here
+        for(int i=0;i<k;i++)
+    {
+        int j=updates[i]-1;
+        a[j]++;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(i>0) a[i]+=a[i-1];
+    }
+    }
