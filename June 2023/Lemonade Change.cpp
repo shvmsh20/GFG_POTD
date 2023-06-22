087f3567bc36dd1648016a8274bf2d284296ef0c
shvmsh20 <itsshivamsharma21@gmail.com>
2023-06-22 23:03:21 +0530

    22 June 2023

---
+++ June 2023/Lemonade Change.cpp
@@ -0,0 +1,23 @@
+bool lemonadeChange(int n, vector<int> &bills) {
+        // code here
+        int five=0,ten=0;
+        for(int i=0;i<n;i++){
+            if(bills[i]==20){
+                if(ten && five){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else return false;
+            }
+            else if(bills[i]==10){
+                ten++;
+                if(five)five--;
+                else return false;
+            }
+            else five++;
+        }
+        return true;
+    }
