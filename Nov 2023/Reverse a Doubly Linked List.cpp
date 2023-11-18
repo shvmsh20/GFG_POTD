0daef5b5629fb779702b7b23b323d19bc3788547
shvmsh20 <itsshivamsharma21@gmail.com>
2023-11-18 22:53:05 +0530

    18 Nov 2023

---
+++ Nov 2023/Reverse a Doubly Linked List.cpp
@@ -0,0 +1,28 @@
+Node* reverseDLL(Node * head)
+    {
+        //Your code here
+         if (head==NULL){
+            return NULL;
+        }
+        if(head->next==NULL){
+            return head;
+        }
+        
+        Node* fast=head;
+        while(fast->next!=NULL){
+            fast=fast->next;
+        }
+        
+        Node* slow=head;
+        while(slow != fast && slow->prev != fast){
+            int temp = slow->data;
+            slow->data= fast->data;
+            fast->data=temp;
+            
+            slow=slow->next;
+            fast=fast->prev;
+            
+        }
+        return head;
+        
+    }
