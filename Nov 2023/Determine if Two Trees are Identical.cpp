6ca6482c3b6adf4888a65c25290d5e61c670bcf8
shvmsh20 <itsshivamsharma21@gmail.com>
2023-11-21 22:48:55 +0530

    21 Nov 2023

---
+++ Nov 2023/Determine if Two Trees are Identical.cpp
@@ -0,0 +1,11 @@
+bool isIdentical(Node *r1, Node *r2)
+    {
+        //Your Code here
+         //Your Code here
+        if(r1==NULL && r2==NULL) return true;
+        if(r1!=NULL && r2!=NULL){
+            return ((r1->data==r2->data) && (isIdentical(r1->left, r2->left)) && (isIdentical(r1->right, r2->right)));
+        }else{
+            return false;
+        }
+    }
