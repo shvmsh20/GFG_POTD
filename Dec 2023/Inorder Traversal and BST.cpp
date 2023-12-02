78fc1e87c67bba0369d1d3d17e9fb50055c277c6
shvmsh20 <itsshivamsharma21@gmail.com>
2023-12-02 12:16:06 +0530

    2 Dec 2023

---
+++ Dec 2023/Inorder Traversal and BST.cpp
@@ -0,0 +1,10 @@
+int isRepresentingBST(int arr[], int N)
+    {
+        // code here
+        for(int i=0; i<N-1; i++){
+            if(arr[i]>=arr[i+1]){
+                return 0;
+            }
+        }
+        return 1;
+    }
