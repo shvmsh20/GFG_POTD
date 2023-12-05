a2b92ed2419929d88d26c03dd22d2e0096a1312c
shvmsh20 <itsshivamsharma21@gmail.com>
2023-12-05 23:29:34 +0530

    05 Dec 2023

---
+++ Dec 2023/Minimize the Heights II.cpp
@@ -0,0 +1,16 @@
+int getMinDiff(int arr[], int n, int k) {
+        // code here
+         sort(arr,arr+n);
+       int minele,maxele;
+       int result=arr[n-1]-arr[0];
+       
+       for(int i=1;i<n;i++){
+           if(arr[i]>=k){
+               maxele=max(arr[i-1]+k,arr[n-1]-k);
+               minele=min(arr[0]+k,arr[i]-k);
+               result=min(result,maxele-minele);
+           }
+           
+       }
+       return result;
+    }
