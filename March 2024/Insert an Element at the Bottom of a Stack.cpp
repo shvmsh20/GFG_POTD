974491b34b3cb33ba9cb1ee81b993595a763c023
shvmsh20 <itsshivamsharma21@gmail.com>
2024-03-24 23:20:02 +0530

    24 March 2024

---
+++ March 2024/Insert an Element at the Bottom of a Stack.cpp
@@ -0,0 +1,15 @@
+stack<int> insertAtBottom(stack<int> st,int x){
+         stack<int> s;
+        while(!st.empty()){
+            s.push(st.top());
+            st.pop();
+        }
+        st.push(x);
+        
+        while(!s.empty()){
+            st.push(s.top());
+            s.pop();
+        }
+        
+        return st;
+    }
