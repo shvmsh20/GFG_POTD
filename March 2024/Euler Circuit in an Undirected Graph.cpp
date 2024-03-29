0e7c8cace139525c82799b44f0fa31cea143c685
shvmsh20 <itsshivamsharma21@gmail.com>
2024-03-29 23:02:25 +0530

    29 March 2024

---
+++ March 2024/Euler Circuit in an Undirected Graph.cpp
@@ -0,0 +1,8 @@
+bool isEularCircuitExist(int v, vector<int>adj[]){
+	    // Code here
+	   for(int i=0;i<v;i++){
+        if(adj[i].size()%2==1)
+            return 0;
+        }
+        return 1;
+	}
