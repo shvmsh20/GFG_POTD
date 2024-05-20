a479237ae264b80e6280dcebcfc4e1cbdd0d72d4
shvmsh20 <itsshivamsharma21@gmail.com>
2024-05-20 22:17:46 +0530

    20 May 2024

---
+++ May 2024/Modular Exponentiation for large numbers.cpp
@@ -0,0 +1,13 @@
+long long int PowMod(long long int x,long long int n,long long int M)
+		{
+		    // Code here
+		    long long int ans=1;
+            while(n>0){
+                if(n&1){
+                    ans =(ans * x) % M;
+                }
+                x=(x*x) % M;
+                n=n/2;
+            }
+           return ans;
+		}
