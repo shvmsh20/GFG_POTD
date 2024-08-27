dd8941ad42fbc8b383ab5743265260f91efb13a6
shvmsh20 <itsshivamsharma21@gmail.com>
2024-08-27 21:23:24 +0530

    27 Aug 2024

---
+++ Aug 2024/Maximum Difference.cpp
@@ -0,0 +1,34 @@
+int findMaxDiff(vector<int> &arr) {
+        // Your code here
+        int n=arr.size();
+        vector<int>left;
+        vector<int>right;
+        stack<int>st,st1;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && arr[st.top()]>=arr[i])
+             st.pop();
+            if(st.empty())
+             left.push_back(0);
+            else
+             left.push_back(arr[st.top()]);
+             st.push(i);
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st1.empty() && arr[st1.top()]>=arr[i])
+             st1.pop();
+            if(st1.empty())
+             right.push_back(0);
+            else
+             right.push_back(arr[st1.top()]);
+             st1.push(i);
+        }
+        reverse(right.begin(),right.end());
+        int maxi=0;
+        for(int i=0;i<n;i++)
+        {
+             maxi=max(maxi,abs(left[i]-right[i]));
+        }
+        return  maxi;
+    }
