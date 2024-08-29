c6447b26a78e33389cb07e396a122cada5a5125f
shvmsh20 <itsshivamsharma21@gmail.com>
2024-08-29 22:38:52 +0530

    29 Aug 2024

---
+++ Aug 2024/Find length of Loop.cpp
@@ -0,0 +1,21 @@
+int countNodesinLoop(Node *head) {
+        // Code here
+        if(head==NULL || head->next==NULL) return 0;
+    Node *slow = head;
+    Node *fast = head;
+    do{
+        slow = slow->next;
+        fast = fast->next->next;
+    }while(fast && fast->next && slow!=fast);
+    if(fast==NULL || fast->next==NULL) return 0;
+    while(fast!=slow){
+        fast = fast->next;
+        slow = slow->next;
+    }
+    int count=0;
+    do{
+        slow = slow->next;
+        count++;
+    }while(slow!=fast);
+    return count;
+    }
