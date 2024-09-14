678f2d6e1e26a127965ccf248515e40b658c5d5a
shvmsh20 <itsshivamsharma21@gmail.com>
2024-09-14 20:59:05 +0530

    14 Sept 2024

---
+++ Sept 2024/Alternate positive and negative numbers.cpp
@@ -0,0 +1,19 @@
+ void rearrange(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector<int> pos;
+	    vector<int> neg;
+	    for(int i=0; i<n; i++){
+	        if(arr[i]>=0){
+	            pos.push_back(arr[i]);
+	        }else{
+	            neg.push_back(arr[i]);
+	        }
+	    }
+	    int i=0, j=0, k=0;
+	    while(i<pos.size() || j<neg.size()){
+	        if(i<pos.size()) arr[k++] = pos[i++];
+	        if(j<neg.size()) arr[k++] = neg[j++];
+	    }
+        
+    }
