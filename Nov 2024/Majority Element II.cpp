25f083b036705ae12394a78527b9e8bdc2324ee2
shvmsh20 <itsshivamsharma21@gmail.com>
2024-11-20 19:53:05 +0530

    20 Nov 2024

---
+++ Nov 2024/Majority Element II.cpp
@@ -0,0 +1,18 @@
+vector<int> findMajority(vector<int>& arr) {
+        // Your code goes here.
+        vector<int> res;
+        int n = arr.size();
+        unordered_map<int, int> mp;
+        for(int i=0; i<n; i++){
+            mp[arr[i]]++;
+        }
+        for(pair<int, int> x: mp){
+            if(x.second>n/3){
+                res.push_back(x.first);
+            }
+        }
+        if(res.size()>1){
+            sort(res.begin(), res.end());
+        }
+        return res;
+    }
