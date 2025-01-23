ee79c5d91e6c3ab241466e05db53074dc3358d6f
shvmsh20 <itsshivamsharma21@gmail.com>
2025-01-23 23:17:39 +0530

    23 Jan 2025

---
+++ Jan 2025/Clone List with Next and Random.cpp
@@ -0,0 +1,35 @@
+Node *cloneLinkedList(Node *head) {
+        // Write your code here
+         if (!head) {
+        return NULL;
+    }
+
+    Node* current = head;
+    while (current) {
+        Node* newNode = new Node(current->data);
+        newNode->next = current->next;
+        current->next = newNode;
+        current = newNode->next;
+    }
+
+    current = head;
+    while (current) {
+        if (current->random) {
+            current->next->random = current->random->next;
+        }
+        current = current->next->next;
+    }
+
+    Node* newHead = head->next;
+    current = head;
+    while (current) {
+        Node* temp = current->next;
+        current->next = temp->next;
+        if (temp->next) {
+            temp->next = temp->next->next;
+        }
+        current = current->next;
+    }
+
+    return newHead;
+    }
