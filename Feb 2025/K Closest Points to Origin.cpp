f48a22b46521b4ad4bba8d4a1648e0e945650488
shvmsh20 <itsshivamsharma21@gmail.com>
2025-02-18 22:56:34 +0530

    18 Feb 2025

---
+++ Feb 2025/K Closest Points to Origin.cpp
@@ -0,0 +1,26 @@
+struct cmp{
+        bool operator()(pair<int, int> &p1, pair<int, int> &p2){
+            return p1.first<p2.first;
+        }
+    };
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // Your code here
+        vector<vector<int>> res;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+        for(int i=0; i<points.size(); i++){
+            int x = points[i][0], y = points[i][1];
+            int dist = (x*x) + (y*y);
+            if(pq.size()<k){
+                pq.push({dist, i});
+            }else if(pq.top().first>dist){
+                pq.pop();
+                pq.push({dist, i});
+            }
+            // cout << pq.top().first << endl;
+        }
+        while(!pq.empty()){
+            res.push_back(points[pq.top().second]);
+            pq.pop();
+        }
+        return res;
+    }
