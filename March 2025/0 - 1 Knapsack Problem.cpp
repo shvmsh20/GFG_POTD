7c809f9f6630991d26a4ac1c1e6faf29f5d19b56
shvmsh20 <itsshivamsharma21@gmail.com>
2025-03-13 09:17:48 +0530

    13 March 2025

---
+++ March 2025/0 - 1 Knapsack Problem.cpp
@@ -0,0 +1,21 @@
+int solve(int w, int n, vector<int> &wt, vector<int> &val, vector<vector<int>> &dp){
+        if(w==0 || n==0){
+            return 0;
+        }
+        if(dp[w][n]!=-1){
+            return dp[w][n];
+        }
+        // ignore
+        int res = solve(w, n-1, wt, val, dp);
+        //take
+        if(w>=wt[n-1]){
+            res = max(res, val[n-1]+solve(w-wt[n-1], n-1, wt, val, dp));
+        }
+        return dp[w][n] = res;
+    }
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        // code here
+        int n = wt.size();
+        vector<vector<int>> dp(W+1, vector<int>(n+1, -1));
+        return solve(W, n, wt, val, dp);
+    }
