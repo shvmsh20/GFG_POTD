86deea960f4a320c9027366a63cc8533ee55f57f
shvmsh20 <itsshivamsharma21@gmail.com>
2025-03-20 22:27:16 +0530

    20 March 2025

---
+++ March 2025/Stock Buy and Sell – Max 2 Transactions Allowed.cpp
@@ -0,0 +1,53 @@
+int maxProfit(vector<int> &price) {
+        // code here
+        int n = price.size();
+            if(n==1)
+            {
+                return 0;
+            }
+            else if(n==2)
+            {
+                int ans = price[1]-price[0];
+                if(ans<=0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return ans;
+                }
+            }
+            vector<int>leri(n);
+            vector<int>rile(n);
+            int mini = price[0];
+            leri[0] = 0;
+            for(int i = 1; i < n; i++)
+            {
+                mini = min(mini,price[i]);
+                leri[i] = max(leri[i-1],price[i]-mini);
+            }
+            int maxi = price[n-1];
+            rile[n-1] = 0;
+            for(int i = n-2; i>=0; i--)
+            {
+                maxi = max(maxi,price[i]);
+                rile[i] = max(rile[i+1],maxi-price[i]);
+            }
+            int ans = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if(i==0)
+                {
+                    ans = max(ans,leri[i]+rile[i+1]);
+                }
+                else if(i==n-1)
+                {
+                    ans = max(ans,leri[i]);
+                }
+                else
+                {
+                    ans = max(ans,leri[i]+rile[i+1]);
+                }
+            }
+            return ans;
+    }
