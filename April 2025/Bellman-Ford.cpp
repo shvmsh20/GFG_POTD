76999af9e978d7848be37c4747a7c8deeeedb971
shvmsh20 <itsshivamsharma21@gmail.com>
2025-04-15 22:34:18 +0530

    15 April 2025

---
+++ April 2025/Bellman-Ford.cpp
@@ -0,0 +1,34 @@
+vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // Code here
+        vector<int> dist(V,1e8);
+        dist[src] = 0;
+        for(int i = 0; i < V-1; i++)
+        {
+            for(auto &it : edges)
+            {
+                int u = it[0];
+                int v = it[1];
+                int w = it[2];
+                
+                if(dist[u] != 1e8 and dist[u] + w < dist[v])
+                {
+                    dist[v] =  dist[u] + w;     
+                }
+                
+            }
+        }
+        
+        for(auto &it : edges)
+        {
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
+                
+            if(dist[u] != 1e8 and dist[u] + w < dist[v])
+            {
+                return {-1};
+            }
+                
+        }
+        return dist;
+    }
