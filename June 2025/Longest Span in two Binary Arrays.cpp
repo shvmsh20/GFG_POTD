3c70ca725932ee45e57778dd13d98e1b16d90130
shvmsh20 <itsshivamsharma21@gmail.com>
2025-06-07 23:08:33 +0530

    07 June 2025

---
+++ June 2025/Longest Span in two Binary Arrays.cpp
@@ -0,0 +1,18 @@
+int longestCommonSum(vector<int> &a1, vector<int> &a2) {
+        // Code here.
+         int n = a1.size();
+        int ans = 0;
+        unordered_map<int, int> ind;
+        
+        ind[0] = -1;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum += (a1[i]-a2[i]);
+            if(ind.find(sum) != ind.end())
+                ans = max(ans, i-ind[sum]);
+            else
+                ind[sum] = i;
+        }
+        
+        return ans;
+    }
