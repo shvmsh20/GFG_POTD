ad04017b439fce5ae2d426c0d0784e68818f9122
shvmsh20 <itsshivamsharma21@gmail.com>
2025-06-18 00:59:13 +0530

    19 June 2025

---
+++ June 2025/Case-specific Sorting of Strings.cpp
@@ -0,0 +1,24 @@
+string caseSort(string& str) {
+        // code here
+        int n = str.size();
+         string up = "";
+        string lo = "";
+        string res = "";
+        for(int i = 0;i < n;i++){
+            if(str[i] >= 'a' && str[i] <= 'z')
+                lo += str[i];
+            else
+                up+= str[i];
+        }
+        sort(up.begin(),up.end());
+        sort(lo.begin(),lo.end());
+        int u = 0;
+        int l = 0;
+        for(int i = 0;i < n;i++){
+            if(str[i] >= 'a' && str[i] <= 'z')
+                res += lo[l++];
+            else
+                res += up[u++];
+        }
+        return res;
+    }
