e2189159cc64351321bbd850d519c85af32db57c
shvmsh20 <itsshivamsharma21@gmail.com>
2025-07-01 10:01:08 +0530

    01 July 2025

---
+++ July 2025/Substrings of length k with k-1 distinct elements.cpp
@@ -0,0 +1,23 @@
+int substrCount(string &S, int K) {
+        // code here
+        unordered_map<char, int> mp;
+        int n = S.length();
+        for(int i=0; i<K; i++){
+            mp[S[i]]++;
+        }
+        int count=0;
+        for(int i=K; i<n; i++){
+            if(mp.size()==(K-1)){
+                count++;
+            }
+            mp[S[i-K]]--;
+            if(mp[S[i-K]]==0){
+                mp.erase(S[i-K]);
+            }
+            mp[S[i]]++;
+        }
+        if(mp.size()==K-1){
+            count++;
+        }
+        return count;
+    }
