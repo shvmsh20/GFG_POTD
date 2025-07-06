ba79606ed4f0566dbf5a364afa85c842cdadb1bd
shvmsh20 <itsshivamsharma21@gmail.com>
2025-07-06 22:55:43 +0530

    06 July 2025

---
+++ July 2025/Maximum Sum Combination.cpp
@@ -0,0 +1,28 @@
+vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
+        // code here
+         int n = a.size();
+        sort(a.rbegin(), a.rend());
+        sort(b.rbegin(), b.rend());
+        priority_queue<pair<int, pair<int, int>>> pq;
+        set<pair<int, int>> vis;
+        pq.push({a[0] + b[0], {0, 0}});
+        vis.insert({0, 0});
+        vector<int> res;
+        while (res.size() < k) {
+            auto top = pq.top();
+            pq.pop();
+            int sum = top.first;
+            int i = top.second.first;
+            int j = top.second.second;
+            res.push_back(sum);
+            if (i + 1 < n && vis.find({i + 1, j}) == vis.end()) {
+                pq.push({a[i + 1] + b[j], {i + 1, j}});
+                vis.insert({i + 1, j});
+            }
+            if (j + 1 < n && vis.find({i, j + 1}) == vis.end()) {
+                pq.push({a[i] + b[j + 1], {i, j + 1}});
+                vis.insert({i, j + 1});
+            }
+        }
+        return res;
+    }
