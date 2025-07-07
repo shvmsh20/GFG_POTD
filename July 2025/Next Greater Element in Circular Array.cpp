26f179b774d146ac0a8c861e7f8f513e91843f83
shvmsh20 <itsshivamsharma21@gmail.com>
2025-07-07 22:01:08 +0530

    07 July 2025

---
+++ July 2025/Next Greater Element in Circular Array.cpp
@@ -0,0 +1,26 @@
+vector<int> nextLargerElement(vector<int> &arr) {
+        int n=arr.size();
+        stack<int> st;
+        vector<int> ans;
+        
+        for(int i=n-1; i>=0; i--) {
+            while(!st.empty() && st.top()<=arr[i])    st.pop();
+            
+            if(st.empty())  ans.push_back(-1);
+            else    ans.push_back(st.top());
+            st.push(arr[i]);
+        }
+        
+        reverse(ans.begin(), ans.end());
+        for(int i=n-1; i>=0; i--) {
+            if(st.empty())  break;
+            if(ans[i]!=-1)  continue;
+            
+            while(!st.empty() && st.top()<=arr[i])    st.pop();
+            if(st.empty())  ans[i]=-1;
+            else    ans[i]=st.top();
+        }
+        
+        return ans;
+
+    }
