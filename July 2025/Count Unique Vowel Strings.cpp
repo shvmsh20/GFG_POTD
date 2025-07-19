545db7592ed12072ace3a99238aae441843a4452
shvmsh20 <itsshivamsharma21@gmail.com>
2025-07-19 23:14:25 +0530

    19 July 2025

---
+++ July 2025/Count Unique Vowel Strings.cpp
@@ -0,0 +1,15 @@
+int vowelCount(string& s) {
+        // code here
+         map<char, int> mp;
+        
+        for(char c: s)
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u') mp[c]++;
+            
+        int sz = mp.size(), ans = sz;
+        
+        
+        for(int i = 2; i<sz; i++)  ans *= i;
+        for(auto it: mp)    ans *= it.second;
+        
+        return ans;
+    }
