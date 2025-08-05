4672a88d9e294ff05215ddd18109640924807260
shvmsh20 <itsshivamsharma21@gmail.com>
2025-08-05 23:03:33 +0530

    05 Aug 2025

---
+++ Aug 2025/Palindrome Sentence.cpp
@@ -0,0 +1,15 @@
+bool isPalinSent(string &s) {
+        // code here
+        string cleaned = "";
+
+        for (char c : s) {
+          if (isalnum(c)) {
+            cleaned += tolower(c);
+          }
+        }
+
+        string reversed = cleaned;
+        reverse(reversed.begin(), reversed.end());
+
+      return cleaned == reversed;
+    }
