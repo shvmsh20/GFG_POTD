8555bceed6d5d581f96197a35a2acc086cccdb0f
shvmsh20 <itsshivamsharma21@gmail.com>
2025-08-08 18:27:54 +0530

    08 Aug 2025

---
+++ Aug 2025/Longest Prefix Suffix.cpp
@@ -0,0 +1,22 @@
+int getLPSLength(string &s) {
+        // code here
+        int n = s.size();
+	    vector<int> LPS(n, 0);
+	    int len=0, i=1;
+	    while(i<n){
+	        if(s[len]==s[i]){
+	            LPS[i] = len+1;
+	            len++;
+	            i++;
+	        }else{
+	            if(len==0){
+	                LPS[i] = 0;
+	                i++;
+	            }else{
+	                len = LPS[len-1];
+	            }
+	        }
+	    }
+	    
+	    return LPS[n-1];
+    }
