711b86f88f1ac3bd7f192853228745d8618b7eac
shvmsh20 <itsshivamsharma21@gmail.com>
2025-08-30 16:25:39 +0530

    30 Aug 2025

---
+++ Aug 2025/The Celebrity Problem.cpp
@@ -0,0 +1,29 @@
+int celebrity(vector<vector<int>>& M) {
+        // code here
+         stack<int> st;
+       int n = M.size();
+    for(int i=0; i<n; i++){
+        st.push(i);
+    }
+    while(st.size()>=2){
+        int a = st.top();
+        st.pop();
+        int b = st.top();
+        st.pop();
+        if(M[a][b]){
+            st.push(b);
+        }else{
+            st.push(a);
+        }
+    }
+    int candid = st.top();
+    for(int i=0; i<n; i++){
+        if(i==candid){
+            continue;
+        }
+        if(M[candid][i] || M[i][candid]==false){
+            return -1;
+        }
+    }
+    return candid;
+    }
