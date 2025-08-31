8dc2381c5fef66363372bbb468b906c9ce8af0bf
shvmsh20 <itsshivamsharma21@gmail.com>
2025-08-31 20:04:21 +0530

    31 Aug 2025

---
+++ Aug 2025/Container With Most Water.cpp
@@ -0,0 +1,15 @@
+int maxWater(vector<int> &A) {
+        // code here
+        long long res = 0;
+         int left = 0, right = A.size()-1;
+         while(left<=right){
+             long long ans = min(A[left], A[right])*(right - left);
+             if(A[left]<=A[right]){
+                 left++;
+             }else{
+                 right--;
+             }
+             res = max(res, ans);
+         }
+         return res;
+    }
