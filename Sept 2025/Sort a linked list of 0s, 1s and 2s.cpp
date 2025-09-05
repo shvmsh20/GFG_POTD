21ba9ea4bc0c2968dca3a05497d7367147bd7964
shvmsh20 <itsshivamsharma21@gmail.com>
2025-09-05 23:20:54 +0530

    05 Sept 2025

---
+++ Sept 2025/Sort a linked list of 0s, 1s and 2s.cpp
@@ -0,0 +1,20 @@
+Node* segregate(Node* head) {
+        // code here
+         vector<int> v;
+        Node* temp=head;
+        while (temp!=nullptr){
+            v.push_back(temp-> data);
+            temp=temp->next;
+        }
+        
+        sort(v.begin(),v.end());
+        
+        Node* newHead = new Node(-1);
+        Node* temp2 = newHead;
+        for (int i=0;i<v.size();i++){
+            Node* n = new Node(v[i]);
+            temp2-> next = n;
+            temp2 = temp2-> next;
+        }
+        return newHead-> next;
+    }
