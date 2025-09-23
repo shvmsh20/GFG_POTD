2d7914308581d6d8a4bec4bcc7843678965b53f3
shvmsh20 <itsshivamsharma21@gmail.com>
2025-09-23 22:33:44 +0530

    23 Sept 2025

---
+++ Sept 2025/Queue Reversal.cpp
@@ -0,0 +1,12 @@
+void reverseQueue(queue<int> &q) {
+        // code here
+        stack<int> st;
+        while(!q.empty()){
+            st.push(q.front());
+            q.pop();
+        }
+        while(!st.empty()){
+            q.push(st.top());
+            st.pop();
+        }
+    }
