ff0c88693db8b432de657ead5005fdc91a6ce10e
shvmsh20 <itsshivamsharma21@gmail.com>
2025-10-10 22:14:25 +0530

    10 Oct 2025

---
+++ Oct 2025/ZigZag Tree Traversal.cpp
@@ -0,0 +1,29 @@
+vector<int> zigZagTraversal(Node* root) {
+        // code here
+         queue<Node*>q;
+        vector<int>v;
+        q.push(root);
+        bool flag=false;
+        while(!q.empty()){
+            int n=q.size();
+            int siz=v.size();
+            while(n--){
+                Node*curr=q.front();
+                q.pop();
+                v.push_back(curr->data);
+                if(curr->left){
+                    q.push(curr->left);
+                }
+                if(curr->right){
+                    q.push(curr->right);
+                }
+            }
+            if(flag){
+                reverse(v.begin()+siz,v.end());
+            }
+            flag=!flag;
+        }
+        
+        return v;
+    	
+    }
