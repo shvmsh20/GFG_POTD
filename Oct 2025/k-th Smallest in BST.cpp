5513ab7399d604c388de7c1000d712f81536470f
shvmsh20 <itsshivamsharma21@gmail.com>
2025-10-15 23:32:54 +0530

    15 Oct 2024

---
+++ Oct 2025/k-th Smallest in BST.cpp
@@ -0,0 +1,9 @@
+ int kthSmallest(Node *root, int &k) {
+        // code here
+       if(root == NULL) return -1;
+        int tmp = kthSmallest(root -> left, k);
+        if(tmp != -1) return tmp;
+        k--;
+        if(k == 0) return root -> data;
+        return kthSmallest(root -> right, k);
+    }
