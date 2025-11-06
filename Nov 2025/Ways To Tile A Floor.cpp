25ac36e0954997bc69c45e7d443161ab87bbf6ac
shvmsh20 <itsshivamsharma21@gmail.com>
2025-11-06 21:58:04 +0530

    06 Nov 2025

---
+++ Nov 2025/Ways To Tile A Floor.cpp
@@ -0,0 +1,10 @@
+int numberOfWays(int n) {
+        // code here
+        vector<int> dp(n+1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
