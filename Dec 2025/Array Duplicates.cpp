11ad39dc2de5c0cf2839034db1a70f4d8075d961
shvmsh20 <itsshivamsharma21@gmail.com>
2025-12-09 23:21:42 +0530

    09 Dec 2025

---
+++ Dec 2025/Array Duplicates.cpp
@@ -0,0 +1,15 @@
+vector<int> findDuplicates(vector<int>& arr) {
+        // code here
+         int n = arr.size();
+        vector<int>ans;
+        for(int i=0;i < n;i++){
+            int idx = abs(arr[i])-1;
+            if(arr[idx] > 0){
+                arr[idx] *= -1;
+            }else{
+                ans.push_back(idx+1);
+            }
+        }
+        
+        return ans;
+    }
