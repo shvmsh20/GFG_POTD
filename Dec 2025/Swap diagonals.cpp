f5f587aca18b95e5ad46a530c9b136aebccf23f0
shvmsh20 <itsshivamsharma21@gmail.com>
2025-12-13 22:48:45 +0530

    13 Dec 2025

---
+++ Dec 2025/Swap diagonals.cpp
@@ -0,0 +1,9 @@
+void swapDiagonal(vector<vector<int>> &mat) {
+        // code here
+         int n= mat.size();
+        for (int i =0;i < n;i++) {
+            swap(mat[i][i],mat[i][n-1-i]);
+            
+            
+        }
+    }
