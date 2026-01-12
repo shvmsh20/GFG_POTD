fa4329a5f7a696be2768792ab04308c86a9afba0
shvmsh20 <itsshivamsharma21@gmail.com>
2026-01-12 09:10:35 +0530

    12 Jan 2026

---
+++ Jan 2026/K Sized Subarray Maximum.cpp
@@ -0,0 +1,23 @@
+struct cmp{
+        bool operator()(pair<int, int> &p1, pair<int, int> &p2){
+            return p1.first<p2.first;
+        }
+    };
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        // code here
+        priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+        int n = arr.size();
+        for(int i=0; i<k; i++){
+            pq.push({arr[i], i});
+        }
+        vector<int> res;
+        res.push_back(pq.top().first);
+        for(int i=k; i<n; i++){
+            pq.push({arr[i], i});
+            while(pq.top().second<=i-k){
+                pq.pop();
+            }
+            res.push_back(pq.top().first);
+        }
+        return res;
+    }
