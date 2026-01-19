ee61bb48e74c3094df053006649ee10f9102cbd5
shvmsh20 <itsshivamsharma21@gmail.com>
2026-01-19 23:17:18 +0530

    19 Jan 2026

---
+++ Jan 2026/Remove K Digits.cpp
@@ -0,0 +1,25 @@
+string removeKdig(string &s, int k) {
+        // code here
+        deque<char>dq;
+         int n=s.length();
+         for(int i=0;i<n;i++){
+            while(!dq.empty()&& dq.back()>s[i] && k>0){
+                if(dq.back()!='0')
+                k--;
+                dq.pop_back();
+            }
+            dq.push_back(s[i]);
+            
+            
+         }
+        while(k-- && !dq.empty()){
+            dq.pop_back();
+        }
+        while(!dq.empty() &&  dq.front()=='0')dq.pop_front();
+        if(dq.size()==0) return "0";
+         string ans;
+         for(auto &x:dq){
+             ans.push_back(x);
+         }
+         return ans;
+    }
