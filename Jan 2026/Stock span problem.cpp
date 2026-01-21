f22fce3c95bc017cbebab78a34e10156614e5bc0
shvmsh20 <itsshivamsharma21@gmail.com>
2026-01-21 22:51:22 +0530

    21 Jan 2026

---
+++ Jan 2026/Stock span problem.cpp
@@ -0,0 +1,19 @@
+vector<int> calculateSpan(vector<int>& arr) {
+        // code here
+         vector<int> ans;
+        stack<int> st;
+        
+        st.push(-1);
+        int n = arr.size();
+        
+        for(int i=0;i<n;i++){
+            while(st.top()!=-1 && arr[i]>=arr[st.top()]){
+                st.pop();
+            }
+            ans.push_back(i-st.top());
+            st.push(i);
+            
+        }
+        
+        return ans;
+    }
