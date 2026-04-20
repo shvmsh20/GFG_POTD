8e835126c89ecd674b6e6bbafd3d0bc1f97f50a9
shvmsh20 <itsshivamsharma21@gmail.com>
2026-04-20 22:31:19 +0530

    20 April 206

---
+++ April 2026/Count Derangements.cpp
@@ -0,0 +1,12 @@
+int derangeCount(int n) {
+        // code here
+        if(n==1) return 0;
+        int prev2=0;
+        int prev1=1;
+        for(int i=2; i<=n; i++){
+            int curr=(i)*(prev2+prev1);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev2;
+    }
