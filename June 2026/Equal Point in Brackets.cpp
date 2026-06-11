7d2013e338be00336843b4671e5c27a1876b56b4
shvmsh20 <itsshivamsharma21@gmail.com>
2026-06-11 22:17:00 +0530

    11 June 2026

---
+++ June 2026/Equal Point in Brackets.cpp
@@ -0,0 +1,17 @@
+int findIndex(string &s) {
+        // code here
+       int rightClosing = 0;
+        for(char c: s){
+            if(c == ')') rightClosing++;
+        }
+        if(rightClosing ==0 ) return 0;
+        if(rightClosing == s.size()) return s.size();
+        int leftOpen = 0;
+        for(int i=0; i<s.size(); i++){
+            char c = s[i];
+            if(leftOpen == rightClosing) return i;
+            if(c=='(') leftOpen++;
+            if(c == ')') rightClosing--;
+        }
+        return s.size();
+    }
